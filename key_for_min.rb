5d97ccc2f8e57fab7996f20861dfa00d813b1bc2
Tan Vo <tan10tvo@gmail.com>
2021-03-06 12:47:16 -0700

    commit tree 41307ebfc7814eabfcaf60dc24e5b92c5676e4c3

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,29 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+#LOWEST VALUE WITHOUT USING .SORT, .MIN, .MIN_BY 
+
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+# def key_for_min_value(name_hash)
+#   lowest_key = nil 
+#   lowest_value = Float::INFINITY 
+#   name_hash.each do |k, v|
+#     if v < lowest_value
+#       lowest_value = v 
+#       lowest_key = k 
+#     end 
+#   end 
+#   lowest_key
+# end
 
 def key_for_min_value(name_hash)
+  lowest_key = nil 
+  lowest_value = nil 
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v 
+      lowest_key = k 
+    end 
+  end 
+  lowest_key
+end 
 
-end
+key_for_min_value(name_hash)
